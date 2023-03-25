4a80dbf9bd6d14098fbdad8e8ee78a0dbc1ef506
a.tliamov <a.tliamov@corp.mail.ru>
2023-03-25 21:27:25 +0300

    Initial commit

---
+++ app/src/main/java/ru/croco/crocodile/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.croco.crocodile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
